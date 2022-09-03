4b2f5530973fe97be56b0235c2e454ffaed89928
Ashish <Ashish@DESKTOP-QJGC2H1>
2022-09-03 11:05:05 +0530

    first commit......

---
+++ src/com/practise/A.java
@@ -0,0 +1,21 @@
+package com.practise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class A {
+public static void main(String[] args) {
+
+
+
+}
+}
+
+
+
